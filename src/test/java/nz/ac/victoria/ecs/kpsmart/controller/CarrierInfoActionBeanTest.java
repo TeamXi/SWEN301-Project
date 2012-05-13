060b2b42262c0657c2ad5b831e2c6fd5b478ee72
danielhodder <danielhodder@gmail.com>
2012-05-13 23:08:00 +0000

    More carrier action bean tests

--- src/test/java/nz/ac/victoria/ecs/kpsmart/controller/CarrierInfoActionBeanTest.java
+++ src/test/java/nz/ac/victoria/ecs/kpsmart/controller/CarrierInfoActionBeanTest.java
@@ -5,8 +5,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 import net.sourceforge.stripes.mock.MockRoundtrip;
 import nz.ac.victoria.ecs.kpsmart.GuiceServletContextListner;
+import nz.ac.victoria.ecs.kpsmart.entities.logging.CarrierDeleteEvent;
 import nz.ac.victoria.ecs.kpsmart.entities.logging.CarrierUpdateEvent;
 import nz.ac.victoria.ecs.kpsmart.entities.state.Carrier;
 import nz.ac.victoria.ecs.kpsmart.integration.EntityManager;
@@ -97,5 +99,45 @@ public class CarrierInfoActionBeanTest extends StripesActionBeanTest {
 		newCarrier.setId(1);
 		verify(this.state).getCarrier(1);
 		verify(this.manager).performEvent(new CarrierUpdateEvent(newCarrier));
+	}
+	
+	@Test
+	public void testUpdateCarrierWithNonExistentIDFals() throws Exception {
+		MockRoundtrip trip = new MockRoundtrip(this.context, CarrierInfoActionBean.class);
+		trip.setParameter("carrierId", "1");
+		trip.setParameter("name", "b");
+		trip.execute("update");
+		assertTrue(trip.getResponse().getOutputString().contains("\"status\":failed"));
+		
+		Carrier newCarrier = new Carrier("b");
+		verify(this.state).getCarrier(1);
+		verify(this.manager, never()).performEvent(new CarrierUpdateEvent(newCarrier));
+	}
+	
+	@Test
+	public void testDelete() throws Exception {
+		Carrier c = new Carrier("a");
+		c.setId(1);
+		when(this.state.getCarrier(1)).thenReturn(c);
+		
+		MockRoundtrip trip = new MockRoundtrip(this.context, CarrierInfoActionBean.class);
+		trip.setParameter("carrierId", "1");
+		trip.execute("delete");
+		assertTrue(trip.getResponse().getOutputString().contains("\"status\":true"));
+		
+		verify(this.state).getCarrier(1);
+		verify(this.manager, times(1)).performEvent(new CarrierDeleteEvent(c));
+	}
+	
+	@Test
+	public void testDeleteOfInvalidCarrierReturnsFalse() throws Exception {
+		MockRoundtrip trip = new MockRoundtrip(this.context, CarrierInfoActionBean.class);
+		trip.setParameter("carrierId", "1");
+		trip.setParameter("name", "b");
+		trip.execute("delete");
+		assertTrue(trip.getResponse().getOutputString().contains("\"status\":false"));
+		
+		verify(this.state).getCarrier(1);
+		verify(this.manager, never()).performEvent(new CarrierDeleteEvent(new Carrier("a")));
 	}
 }
