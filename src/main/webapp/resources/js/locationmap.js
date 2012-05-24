e7599c87dd5bfce1e2b876775e6a46647e2cf3ed
Melby Ruarus <melby.ruarus@gmail.com>
2012-05-24 22:24:42 +0000

    Commended locationmap/mail.js

--- src/main/webapp/resources/js/locationmap.js
+++ src/main/webapp/resources/js/locationmap.js
@@ -1,16 +1,30 @@
 KPS.event = KPS.event || {};
 KPS.event.location = KPS.event.location || {};
 
+/**
+ * KPS.event.location
+ * 
+ * The location map stuff
+ */
 (function(cls, $, undefined) {
 	var globalMap = undefined;
 	var infoWindow = undefined;
 	
+	/**
+	 * Initialize the map
+	 */
 	function createWorldMap() {
 		globalMap = KPS.util.map.newInstance(document.getElementById('allLocationsMap'), 4, -34.397, 150.644);
 		
+		// Custom info window
 		infoWindow = new InfoBubble(globalMap);
 	};
 	
+	/**
+	 * Show the routes to/from a location
+	 * 
+	 * @param name the name of the location
+	 */
 	function showRoutesFor(name) {
 		$.get("location?connected", {name: name}, function(data) {
 			var list = eval(data);
@@ -31,10 +45,16 @@ KPS.event.location = KPS.event.location || {};
 		});
 	}
 	
+	/**
+	 * Hide/remove all the route lines
+	 */
 	function hideRoutes() {
 		KPS.util.map.removePolylines(globalMap);
 	}
 	
+	/**
+	 * Add all the markers to the map
+	 */
 	function loadLocations(allLocations) {
 		for(locIdx in allLocations){
 			(function(location) {
@@ -42,6 +62,8 @@ KPS.event.location = KPS.event.location || {};
 					map: globalMap, 
 					position: new google.maps.LatLng(location.latitude, location.longitude)
 				});
+				
+				// Setup hover actions, like customer popover and route lines
 				google.maps.event.addListener(marker, "mouseover", function(event) {
 					infoWindow.setContent(location.name);
 					infoWindow.setPosition(event.latLng);
@@ -56,7 +78,6 @@ KPS.event.location = KPS.event.location || {};
 				});
 			}(allLocations[locIdx]));
 		}
-
 	};
 	
 	$(document).ready(function () {
--- src/main/webapp/resources/js/mail.js
+++ src/main/webapp/resources/js/mail.js
@@ -1,44 +1,64 @@
 KPS.event = KPS.event || {};
 KPS.event.maildelivery = KPS.event.maildelivery || {};
 
+/**
+ * KPS.event.maildelivery
+ * 
+ * The mail screen js
+ */
 (function (cls, $, undefined) {
 	// TODO: el, $el?
 	
+	/**
+	 * The initial modal configuration
+	 */
 	var modalConfiguration = {
 			title: "New mail delivery",
 			okButton: {
 				title: "Submit",
 				action: function() {
+					// Submit the form
 					document.getElementById('newMailForm').submit();
 				}
 			}
 	};
 	
+	/**
+	 * The quote screen modal configuration
+	 */
 	var doneModalConfiguration = {
 			title: "Delivery summary",
 			okButton: {
 				title: "Confirm",
 				action: function() {
+					// Confirm the submission
 					submitNewMailForm('newMailForm');
 				}
 			},
 			cancelButton: {
 				title: "Back",
 				action: function() {
+					// Move back to first form
 					KPS.modal.configure(modalConfiguration);
 					KPS.modal.carrousel.show(0);
 				}
 			}
 	};
 	
+	/**
+	 * Submit the new action, ie confirm the mail delivery
+	 */
 	function submitNewMailForm(id) {
 		var form = document.getElementById(id);
 		
-		if(!validateForm(form)) return;KPS.util.submitForm(form, KPS.siteRoot+"/event/mail?new", function(data){
+		if(!validateForm(form)) return;
+		
+		KPS.util.submitForm(form, KPS.siteRoot+"/event/mail?new", function(data){
 			var returnObj = eval(data);
 			var status = returnObj.status;
 			
 			if(!status){
+				// Errors, go back and show them
 				KPS.validation.validationErrors(form, returnObj.validation);
 				KPS.modal.configure(modalConfiguration);
 				KPS.modal.carrousel.show(0);
@@ -48,27 +68,34 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		});
 	};
 	
+	/**
+	 * Submit the form for a quote
+	 */
 	cls.quoteForm = function(id) {
 		var form = document.getElementById(id);
 		
 		if(!validateForm(form)) return;
+		
 		KPS.util.submitForm(form, KPS.siteRoot+"/event/mail?quote", function(data){
 			var returnObj = eval(data);
 			var status = returnObj.status;
 			
 			if(!status){
+				// Errors, show them
 				KPS.validation.validationErrors(form, returnObj.validation);
 			}else{
+				// ALl good, slide to next screen
+				
 				KPS.modal.configure(doneModalConfiguration);
 				KPS.modal.carrousel.show(1);
 				
-				
-				
+				// Set up the map & route summary
 				map = setUpMap();
 				KPS.util.map.removePolylines(map);	
 				KPS.util.map.removeMarkers(map);
 				applyRoutesOverlay(map, returnObj.data.summary.route);
 				
+				// Fill in the quote info
 				document.getElementById('mail-success-info-revenue').innerHTML = returnObj.data.summary.revenue;
 				document.getElementById('mail-success-info-expenditure').innerHTML = returnObj.data.summary.expenditure;
 				document.getElementById('mail-success-info-delivery-duration').innerHTML = returnObj.data.summary.duration;
@@ -78,6 +105,9 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		
 	var summaryMap = undefined;
 	
+	/**
+	 * Add the markers and route lines to the summary map
+	 */
 	function applyRoutesOverlay(map, routes){
 		var bounds = new google.maps.LatLngBounds();
 		var startPos;
@@ -102,6 +132,9 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		map.fitBounds(bounds);
 	}
 	
+	/**
+	 * Initialize the map
+	 */
 	function setUpMap() {
 		if(!summaryMap){
 			summaryMap = KPS.util.map.newInstance(document.getElementById('mail-success-info-route-map'), 1, 0, 0);
@@ -109,6 +142,9 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		return summaryMap;
 	}
 	
+	/**
+	 * Validate the form for submission
+	 */
 	function validateForm(form) {
 		var ok = true;
 		KPS.validation.resetValidation(form);
@@ -124,6 +160,9 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		return ok;
 	}
 	
+	/**
+	 * Validate a location field
+	 */
 	function validateLocationField(element) {
 		if(!KPS.data.locations.exists(element.value)) {
 			KPS.validation.validationError(element, '"'+element.value+'" is not a valid location');
@@ -132,7 +171,11 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		return true;
 	}
 	
+	/**
+	 * Update the avalible priorities in the dropdown
+	 */
 	function updatePriorityDropdown() {
+		// Disable all the priorities
 		$("#newMailForm select[name='priority'] option[value!='placeholder']").each(function(index, child) {
 			child.disabled= true;
 		});
@@ -142,39 +185,54 @@ KPS.event.maildelivery = KPS.event.maildelivery || {};
 		var destination = form.elements['destination'].value;
 		var priorityElm = form.elements['priority'];
 		
+		// Reset the form validation
 		KPS.validation.resetValidation(form);
 		
+		// Function that show the falied errors
 		function dontDeliverMessage() {
 			KPS.validation.validationError(form.elements['destination'], "KPS does not deliver mail from "+source+" to "+destination);
 		}
 		
 		if(KPS.data.locations.exists(source) && KPS.data.locations.exists(destination)) {
+			// If both locations valid
+			
 			if(KPS.data.locations.isInternational(source) && KPS.data.locations.isInternational(destination)) {
+				// Fail if both international
+				
 				dontDeliverMessage();
 			}
 			else {
+				// Looks valid client-side, query server
+				
+				// Show the spinner
 				$("#select-spinner").show();
 				$.get(KPS.siteRoot+"/event/mail?availablepriorities",{source:source, destination:destination},function(data){
 					var priorities = eval(data);
 					if (priorities.length == 0){
+						// No priorities valid, show error
 						dontDeliverMessage();
 					}
 					else{
+						// Some priorities valid, enable those in dropdown
+						
 						$("option[value!='placeholder']", priorityElm).each(function(index, child) {
-							child.disabled= priorities.indexOf(child.value)<0;
+							child.disabled = priorities.indexOf(child.value)<0;
 						});
 					}
 					
+					// Hide the spinner
 					$("#select-spinner").hide();
 				});
 			}
 		}
 		else if(source && destination){
+			// If both locations are defined, show the standard location validation errors
 			validateLocationField(form.elements['source']);
 			validateLocationField(form.elements['destination']);
 		}
 		
-		if($("option:selected", priorityElm).is(":disabled")) { // If selected option is disabled select the placeholder
+		// If selected option is disabled select the placeholder
+		if($("option:selected", priorityElm).is(":disabled")) {
 			priorityElm.value = "placeholder";
 		}
 	}
